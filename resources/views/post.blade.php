b31764e7fa9e4519e1868913d5e48bff134aaf6d
Salomo Jefry <salomojefry@example.com>
2025-01-14 11:29:29 +0700

    edit view data

---
+++ resources/views/post.blade.php
@@ -0,0 +1,26 @@
+<x-layout>
+  <x-slot:title>{{ $title }}</x-slot:title>
+  
+
+
+
+  
+
+<article class="py-8 max-w-screen-md border-b border-gray-300">
+
+  <h2 class="mb-1 text-3xl tracking-light font-bold text-gray-900">{{ $post['title'] }}</h2>
+
+  <div class="text-base text-gray-500">
+    <a href="#">{{ $post['author'] }}</a> | 13 Januari 2025
+  </div>
+  <p class="my-4 font-light">
+    {{ $post['body'] }}
+  </p>
+  <a href="/posts" class="font-medium text-blue-500">&laquo; Back to Post</a>
+</article>
+
+
+
+ 
+
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,9 +11,55 @@ Route::get('/about', function () {
     return view('about', ['title' => 'About']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function () {
+    return view('posts', [
+        
+       
+        'title' => 'Blog',
+        'posts' => [
+            [
+                'id' => '1',
+                'slug' => 'post-1',
+                'title' => 'Post 1',
+                'author' => 'John Doe',
+                'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus cupiditate nobis ut aspernatur praesentium! Praesentium eveniet error reiciendis tempora id? Eos accusantium nobis voluptas. Culpa illo accusantium nam sint mollitia!',
+            ],
+            [
+                'id'=> '2',
+                'slug'=> 'post-2',
+                'title' => 'Post 2',
+                'author' => 'Jane Smith',
+                'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus cupiditate nobis ut aspernatur praesentium! Praesentium eveniet error reiciendis tempora id? Eos accusantium nobis voluptas. Culpa illo accusantium nam sint mollitia!',
+            ],
+        ]]);
 });
+
+
+Route::get('/posts/{slug}', function ($slug) {
+    $posts = [
+        [
+            'id' => '1',
+            'slug'=> 'post-1',
+            'title' => 'Post 1',
+            'author' => 'John Doe',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus cupiditate nobis ut aspernatur praesentium! Praesentium eveniet error reiciendis tempora id? Eos accusantium nobis voluptas. Culpa illo accusantium nam sint mollitia!',
+        ],
+        [
+            'id'=> '2',
+            'slug'=> 'post-2',
+            'title' => 'Post 2',
+            'author' => 'Jane Smith',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus cupiditate nobis ut aspernatur praesentium! Praesentium eveniet error reiciendis tempora id? Eos accusantium nobis voluptas. Culpa illo accusantium nam sint mollitia!',
+        ],
+    ];
+
+    $post = Arr::first($posts, function($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
+});
+
 
 Route::get('/contact', function () {
     return view('contact', ['title'=> 'Contact']);
